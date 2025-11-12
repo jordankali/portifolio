f727b474825e5e42e87506161cffa2bbba4dbeb6
jordankali <jorammalley@gmail.com>
2025-11-12 03:32:44 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:"./portifolio",
+    base:"/portifolio",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
